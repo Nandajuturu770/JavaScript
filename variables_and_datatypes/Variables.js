15dbe41eeed1e314050a80da7b6bbb4b49212f05
Nandajuturu770 <nandajuturu@gmail.com>
2025-11-04 22:49:17 +0530

    first commit

---
+++ variables_and_datatypes/Variables.js
@@ -0,0 +1,18 @@
+/**
+ * VARIABLES
+ *
+ * What is variable?
+ * Variables are containers used to store data.
+ *
+ * How many ways can we declare variables?
+ * var   - for function-scoped or globally-scoped variables (older syntax).
+ * let   - for block-scoped variables that can be changed later.
+ * const - for block-scoped variables that should not change once set.
+ *
+ * Variable Naming Conventions
+ * Must start with a letter, underscore (_), or dollar sign ($)
+ * Can include letters, digits, underscores, or dollar signs
+ * Are case-sensitive (myVar is different from myvar)
+ * Avoid using reserved keywords as variable names
+ *
+ */
